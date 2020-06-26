84790b7a22a93c4fdef20062e4759560228fa780
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-26 09:03:15 +0100

    new animation page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Animations',
       theme: ThemeData(
-
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
@@ -26,86 +25,87 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-Animation animation;
-AnimationController animationController;
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  Animation animation, delayedAnimation, muchDelayedAnimation;
+  AnimationController animationController;
 
-@override
-void initState() {
-  // TODO: implement initState
-  super.initState();
-  animationController = AnimationController(duration: Duration(seconds: 4), vsync: this);
-  animation = Tween(begin: 0.0, end: -0.15).animate(CurvedAnimation(
-      parent: animationController, curve: Curves.easeOut));
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+    animation = Tween(begin: -1.0, end: 0.0).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.fastOutSlowIn));
+    delayedAnimation = Tween(begin: -1.0, end: 0.0).animate(
+        CurvedAnimation(parent: animationController, curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn)));
+    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(
+        CurvedAnimation(parent: animationController, curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn)));
+  }
 
-}
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
 
-  final width = MediaQuery.of(context).size.width;
-
-  return AnimatedBuilder(
-    animation: animationController,
-    builder: (BuildContext context, Widget child){
-      return Scaffold(
-          body: Center(
-              child: Stack(
-                children: <Widget>[
-                  Center(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: 350,
-                      height: 200,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15)
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          RaisedButton(
-                            onPressed: (){
-
-                            },
-                            elevation: 7.0,
-                            textColor: Colors.white,
-                            child: Text('Ok'),
-                            color: Colors.green,
-                          ),
-                          RaisedButton(
-                            onPressed: (){
-
-                            },
-                            elevation: 7.0,
-                            textColor: Colors.white,
-                            child: Text('Cancel'),
-                            color: Colors.green,
-                          )
-                        ],
-                      ),
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (BuildContext context, Widget child) {
+        return Scaffold(
+            body: Center(
+                child: Stack(
+          children: <Widget>[
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: 350,
+                height: 200,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () {},
+                      elevation: 7.0,
+                      textColor: Colors.white,
+                      child: Text('Ok'),
+                      color: Colors.green,
                     ),
-                  ),
-                  Center(
-                      child: GestureDetector(
-                        child: Container(
-                          alignment: Alignment.bottomCenter,
-                          width: 350,
-                          height: 200,
-                          decoration: BoxDecoration(
-                              color: Colors.purpleAccent,
-                              borderRadius: BorderRadius.circular(15)
-                          ),
-                          transform: Matrix4.translationValues(0.0, animation.value * width, 0.0),
-                        ),
-                      )
-                  )
-                ],
-              )
-          )
-
-      );
-    },
-  );
+                    RaisedButton(
+                      onPressed: () {},
+                      elevation: 7.0,
+                      textColor: Colors.white,
+                      child: Text('Cancel'),
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Center(
+                child: GestureDetector(
+                  onTap: (){
+                    animationController.forward();
+                  },
+                  onDoubleTap: (){
+                    animationController.reverse();
+                  },
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                width: 350,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.purpleAccent,
+                    borderRadius: BorderRadius.circular(15)),
+                transform: Matrix4.translationValues(
+                    0.0, animation.value * width, 0.0),
+              ),
+            ))
+          ],
+        )));
+      },
+    );
+  }
 }
-}
-
